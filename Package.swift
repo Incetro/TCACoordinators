476126773e43d7551443799e429b20c45b54bf07
Gleb Kovalenko <yourpendos@gmail.com>
2024-09-12 16:49:39 +0300

    feat: update tca version

--- Package.swift
+++ Package.swift
@@ -15,14 +15,14 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/johnpatrickmorgan/FlowStacks", "0.3.6" ..< "0.6.0"),
-    .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.8.0"),
+    .package(url: "https://github.com/Incetro/TCA", .branch("main"))
   ],
   targets: [
     .target(
       name: "TCACoordinators",
       dependencies: [
         .product(name: "FlowStacks", package: "FlowStacks"),
-        .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
+        .product(name: "ComposableArchitecture", package: "TCA"),
       ]
     ),
     .testTarget(
